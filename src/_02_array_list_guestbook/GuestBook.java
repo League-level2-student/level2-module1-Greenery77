e36d215b38910f1ba903ac49d8eef17161d7a4f9
root <root@0954d56cf1af>
2021-11-01 23:55:28 +0000

    Finish guestbook

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,7 +1,70 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	
+	JButton add = new JButton();
+	 
+	 JButton view = new JButton();
+	 
+	 ArrayList<String> names = new ArrayList<String>();
+	 
+	public void createUI(){
+	
+	 JFrame frame = new JFrame();
+	 
+	 frame.show();
+
+	 JPanel panel = new JPanel();
+	 
+	 frame.add(panel);
+	 
+	 panel.add(add);
+	 
+	 panel.add(view);
+	 
+	 add.addActionListener(this);
+	 
+	 add.setLabel("Add Name");
+	 
+	 view.addActionListener(this);
+	
+	 view.setLabel("View Names");
+	 
+	 frame.pack();
+	 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() == add) {
+			
+			String name = JOptionPane.showInputDialog("Enter a name.");
+			
+			names.add(name);
+			
+		}
+		
+		if (e.getSource() == view) {
+			
+			for(String s : names){
+				System.out.println("Guest #" +  +  s);
+			}
+			
+		}
+		
+	}
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
---
+++ src/_02_array_list_guestbook/GuestBookRunner.java
@@ -0,0 +1,13 @@
+package _02_array_list_guestbook;
+
+public class GuestBookRunner {
+
+	public static void main(String[] args) {
+		
+		GuestBook gb = new GuestBook();
+		
+		gb.createUI();
+		
+	}
+	
+}
